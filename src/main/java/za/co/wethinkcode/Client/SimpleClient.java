868f7406159276187170485327def9e830b976c9
kwmbona022 <kwmbona022@student.wethinkcode.co.za>
2023-04-14 15:32:25 +0200

    user input

--- src/main/java/za/co/wethinkcode/Client/SimpleClient.java
+++ src/main/java/za/co/wethinkcode/Client/SimpleClient.java
@@ -15,13 +15,7 @@ public class SimpleClient implements Serializable{
     static Scanner scanner;
 
     public static void main(String[] args) {
-//        if (args.length != 2) {
-//            System.out.println("java ClientExample <ipAddress> <port>");
-//            return;
-//        }
-//
-//        String ipAddress = args[0];
-//        int port = Integer.parseInt(args[1]);
+
         try (
 
                 Socket socket = new Socket("localhost", 5000);
@@ -30,12 +24,18 @@ public class SimpleClient implements Serializable{
                 OutputStream outputStream = socket.getOutputStream();
                 BufferedReader in = new BufferedReader(new InputStreamReader(
                         socket.getInputStream()));
+                BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
         )
         {
+            //get the user input
+            System.out.println("Greeting");
+              String message = userInput.readLine();
+            System.out.println("Name of the Robot");
+              String name = userInput.readLine();
+            System.out.println("give command");
+              String command = userInput.readLine();
 
             scanner = new Scanner(System.in);
-            String name = getInput("What do you want to name your robot?");
-            String command = getInput(  "> What must I do next?").strip().toLowerCase();
 
             SimpleClient simpleClient = new SimpleClient();
             String[] arrayArgs;
@@ -50,7 +50,7 @@ public class SimpleClient implements Serializable{
             //SEND REQUEST
             outputStream.write(requestData);
             outputStream.flush();
-
+            out.println(message);
             byte[] data = new byte[1024];
             // receiving message request from client
             InputStream inputStream = socket.getInputStream();
@@ -62,24 +62,13 @@ public class SimpleClient implements Serializable{
             //ObjectMapper objectMapper = new ObjectMapper();
             JsonNode jsonNode = objectMapper.readTree(jsonData);
             System.out.println(jsonNode);
-
+            String response = in.readLine();
+            System.out.println("Server response: " + response);
 
         } catch (IOException e) {
             e.printStackTrace();
-//        } catch (ClassNotFoundException e) {
-//            throw new RuntimeException(e);
-//        }
-    }}
 
-    private static String getInput(String prompt) {
-        System.out.println(prompt);
-        String input = scanner.nextLine();
-        while (input.isBlank()) {
-            System.out.println(prompt);
-            input = scanner.nextLine();
-        }
-        return input;
-    }
+    }}
 
     private JSONObject requestMessage(String robotName, String commandName, String arguments)  {
         JSONObject jsonRequest = new JSONObject();
