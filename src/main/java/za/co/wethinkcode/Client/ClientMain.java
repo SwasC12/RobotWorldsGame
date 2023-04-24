99417d38ce46fa6da8b3f03c778178acfbe67c47
njmbanj022 <njmbanj022@student.wethinkcode.co.za>
2023-04-24 13:13:28 +0200

    ClientMain updated

--- src/main/java/za/co/wethinkcode/Client/ClientMain.java
+++ src/main/java/za/co/wethinkcode/Client/ClientMain.java
@@ -23,9 +23,9 @@ public class ClientMain implements Serializable {
 //        int port = Integer.parseInt(args[1]);
 
         try (
-            //Socket socket = new Socket(ipAddress,port))
-             Socket socket = new Socket("localhost", 5000))
-            //Socket socket = new Socket("localhost", 5000))
+                // Socket socket = new Socket(ipAddress,port))
+                // Socket socket = new Socket("10.3.16.115", 5000))
+                Socket socket = new Socket("localhost", 5000))
         {
             System.out.println("Waiting for connection response from server: ");
             Thread.sleep(4000);
@@ -42,66 +42,76 @@ public class ClientMain implements Serializable {
             System.out.println("--------------------------------------------------------");
             System.out.println("-----------------WELCOME TO ROBOT WORLD-----------------");
             System.out.println("--------------------------------------------------------");
-          //  System.out.println("Do you want to launch a robot? (yes/no): ");
+            //  System.out.println("Do you want to launch a robot? (yes/no): ");
             // Getting response from user
 //            String launchRobot = userInput.readLine();
 
-            if (getInput("Do you want to launch a robot? (yes/no): ").equalsIgnoreCase("yes")) {
-                CreateJSONObject createJSONObject = new CreateJSONObject();
-              //  System.out.println("Please enter the name of the robot:");
-                createJSONObject.setRobotName(getInput("Please enter the name of the robot:"));
+            while (true){
+                String userInput = getInput("Do you want to launch a robot? (yes/no): ");
 
-                System.out.println("Client launching " + createJSONObject.getRobotName() + "...");
-                Thread.sleep(3000);
+                if (userInput.equalsIgnoreCase("yes")) {
+                    CreateJSONObject createJSONObject = new CreateJSONObject();
+                    //  System.out.println("Please enter the name of the robot:");
+                    createJSONObject.setRobotName(getInput("Please enter the name of the robot:"));
 
-                // code to launch the robot
-                JsonNode response = client.sendRequestToServer(createJSONObject.getJsonObject() ,socket);
-                //display response on console
-                ConsoleDisplayServerResponse.displayResponse(response, createJSONObject.getCommand());
+                    System.out.println("Client launching " + createJSONObject.getRobotName() + "...");
+                    Thread.sleep(3000);
 
-                //check if the launch was successfully
-                if (response.get("result").asText().equalsIgnoreCase("OK")){
-                    while (socket.isConnected()) {
+                    // code to launch the robot
+                    JsonNode response = client.sendRequestToServer(createJSONObject.getJsonObject() ,socket);
+                    //display response on console
+                    ConsoleDisplayServerResponse.displayResponse(response, createJSONObject.getCommand());
+
+                    //check if the launch was successfully
+                    if (response.get("result").asText().equalsIgnoreCase("OK")){
+                        while (socket.isConnected()) {
 //                        System.out.println(createJSONObject.getRobotName() + "> What must I do next?");
-                        String command = getInput(createJSONObject.getRobotName() + "> What must I do next?").strip().toLowerCase();
+                            String command = getInput(createJSONObject.getRobotName() + "> What must I do next?");
 
-                        if (command.equalsIgnoreCase("quit")){
-                            out.println("Shutting down "+createJSONObject.getRobotName()+"...");
-                            socket.close();
-                            break;
-                        }
+                            if (command.equalsIgnoreCase("quit")){
+                                out.println("Shutting down "+createJSONObject.getRobotName()+"...");
+                                socket.close();
+                                break;
+                            }
 
-                        createJSONObject.setCommand(command);
-                        if (createJSONObject.getCommand().equalsIgnoreCase("help")){
-                            helpCommand helpCommand = new helpCommand();
-                            System.out.println(helpCommand);
+                            createJSONObject.setCommand(command);
+                            if (createJSONObject.getCommand().equalsIgnoreCase("help")){
+                                helpCommand helpCommand = new helpCommand();
+                                System.out.println(helpCommand);
 
-                        } else if (!createJSONObject.getCommand().equalsIgnoreCase("launch")){
-                            //send request to server and receive response
-                             response = client.sendRequestToServer(createJSONObject.getJsonObject(),socket);
-                            //display response on console
-                            System.out.println("Waiting for the response from the server");
-                            Thread.sleep(3000);
-                             ConsoleDisplayServerResponse.displayResponse(response, createJSONObject.getCommand());
-                        }
-                        else {
-                            System.out.println("You already launched your robot!!!");
+                            } else if (!createJSONObject.getCommand().equalsIgnoreCase("launch")){
+                                //send request to server and receive response
+                                response = client.sendRequestToServer(createJSONObject.getJsonObject(),socket);
+                                //display response on console
+                                System.out.println("Waiting for the response from the server");
+                                Thread.sleep(3000);
+                                ConsoleDisplayServerResponse.displayResponse(response, createJSONObject.getCommand());
+                            }
+                            else  {
+                                System.out.println("You already launched your robot!!!");
+                            }
                         }
                     }
+                } else if (userInput.equalsIgnoreCase("no")) {
+                    System.out.println("Okay, bye...");
+                    return;
+                }else {
+                    System.out.println("Invalid input, please enter yes/no");
+                    continue;
                 }
-            } else {
-                System.out.println("Okay, bye...");
+                break;
             }
 
+
         } catch (InterruptedException | ClassNotFoundException | ConnectException e) {
-                System.err.println("Failed to connect to server: " + e.getMessage());
+            System.err.println("Failed to connect to server: " + e.getMessage());
 //            throw new RuntimeException(e);
         }catch (IOException e){
             System.err.println("Error : "+ e.getMessage());
         }
     }
 
-    private static String getInput(String prompt) throws IOException {
+    public static String getInput(String prompt) throws IOException {
         System.out.println(prompt);
 //        String input = scanner.nextLine();
         BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
@@ -110,7 +120,7 @@ public class ClientMain implements Serializable {
             System.out.println(prompt);
             input = userInput.readLine();
         }
-        return input;
+        return input.strip().toLowerCase();
     }
 
 }
