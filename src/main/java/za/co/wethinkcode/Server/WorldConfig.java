a38609b25a725aad156d42cca69fc15e31adcb06
swchatt022 <swchatt022@student.wethinkcode.co.za>
2023-05-02 11:19:13 +0200

    switched from .properties to json

--- src/main/java/za/co/wethinkcode/Server/WorldConfig.java
+++ src/main/java/za/co/wethinkcode/Server/WorldConfig.java
@@ -1,74 +1,25 @@
 package za.co.wethinkcode.Server;
 import java.io.*;
 import java.util.Properties;
+import org.json.simple.*;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 public class WorldConfig{
     //configuration file for world
-    public void MakeConfigFile(){
-
-        Properties prop = new Properties();
-        OutputStream output = null;
-
-        try {
-            //hidden config file
-            output = new FileOutputStream(".config.properties");
-
-            //set the properties value
-            //height and width
-
-            prop.setProperty("height","500");
-            prop.setProperty("width","500");
-
-            //save the properties to the file
-            prop.store(output,null);
-
-        }catch (IOException io){
-            io.printStackTrace();
-        }finally {
-            if (output != null){
-                try{
-                    output.close();
-                }catch (IOException e){
-                    e.printStackTrace();
-                }
-            }
-        }
-
-    }
-
-
-
-    public void readConfigFile(){
-        //load and read config file at server start
-        Properties prop = new Properties();
-        InputStream input = null;
-
+    public void ReadConfigFile(){
+        JSONParser parser = new JSONParser();
         try{
-            input = new FileInputStream("config.properties");
+            FileReader reader = new FileReader("src/main/java/za/co/wethinkcode/Server/config.json");
+            JSONObject data = (JSONObject) parser.parse(reader);
 
-            //load the properties from the file
-            prop.load(input);
+            int height = Integer.parseInt(data.get("height").toString());
+            int width = Integer.parseInt(data.get("width").toString());
 
-            //Get the property values
-            String height = prop.getProperty("height");
-            String width = prop.getProperty("width");
-
-
-            //make use of the height and width
-            //height and width load in server
-            //to implement here
-        }catch (IOException io){
-            io.printStackTrace();
-        } finally {
-            if (input != null){
-                try{
-                    input.close();
-                }catch (IOException e){
-                    e.printStackTrace();
-                }
-            }
+            reader.close();
+        }catch (IOException | ParseException e){
+            e.printStackTrace();
         }
 
     }
-
 }
