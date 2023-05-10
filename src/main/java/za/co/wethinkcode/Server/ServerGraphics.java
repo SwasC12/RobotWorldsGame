7e4bf4803253cc02ff18e701e0a501ba45e8e826
swchatt022 <swchatt022@student.wethinkcode.co.za>
2023-05-10 08:52:30 +0200

    terminal colours and progress bars for server

---
+++ src/main/java/za/co/wethinkcode/Server/ServerGraphics.java
@@ -0,0 +1,9 @@
+package za.co.wethinkcode.Server;
+
+public class ServerGraphics {
+
+    public static final String ANSI_RED = "\u001b[31;1m";
+    public static final String ANSI_GREEN = "\\u001b[32;1m";
+
+    //loading/progress bars below
+}
