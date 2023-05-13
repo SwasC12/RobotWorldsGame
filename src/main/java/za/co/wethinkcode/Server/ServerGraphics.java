ba4bc7b82b6727dc53f41b549f0eb9d3a426d021
Swasteer Chattergoon <swchatt022@student.wethinkcode.co.za>
2023-05-13 23:21:53 +0200

    updated

--- src/main/java/za/co/wethinkcode/Server/ServerGraphics.java
+++ src/main/java/za/co/wethinkcode/Server/ServerGraphics.java
@@ -9,6 +9,7 @@ public class ServerGraphics {
 
     public static final String ANSI_CYAN_BG = "\u001b[46m";
     public static final String ANSI_CYAN_BG_BRIGHT = "\u001b[46;1m";
+    public static final String ANSI_YELLOW_BG = "\u001b[43;1m";
 
     //loading/progress bars below
 }
