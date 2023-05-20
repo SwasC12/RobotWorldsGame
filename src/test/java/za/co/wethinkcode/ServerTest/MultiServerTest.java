8f8b0669238104f9d2135e98adbee804c0dee367
Swasteer Chattergoon <swchatt022@student.wethinkcode.co.za>
2023-05-20 23:11:02 +0200

    multi server test, passing, fix!

--- src/test/java/za/co/wethinkcode/ServerTest/MultiServerTest.java
+++ src/test/java/za/co/wethinkcode/ServerTest/MultiServerTest.java
@@ -1,5 +1,23 @@
 package za.co.wethinkcode.ServerTest;
+import org.junit.Test;
 
-public class MultiServerTest {
+import za.co.wethinkcode.Server.MultiServers;
+
+public class MultiServerTest {//broken test
+    @Test
+    public void testWorldValues(){
+        String args = "";//get user input from multiServers
+        if (args.length() == 0 ){
+            System.out.println("You did not enter any configuration parameters thus we will use default values.");
+        }
+        else if (args.length() < 6 && args.length() >0 ){
+            System.out.println("You entered less than the required number of configuration parameters thus we will use default values." );
+    
+        }
+
+    }
+    
+
+
 
 }
