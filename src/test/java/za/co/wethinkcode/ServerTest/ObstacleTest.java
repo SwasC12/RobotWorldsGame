8791a7dd255649f4231c9fc96bd6c7bde0a1e8ab
kwmbona022 <kwmbona022@student.wethinkcode.co.za>
2023-05-21 23:58:50 +0200

    tests for path blocked

--- src/test/java/za/co/wethinkcode/ServerTest/ObstacleTest.java
+++ src/test/java/za/co/wethinkcode/ServerTest/ObstacleTest.java
@@ -1,8 +1,15 @@
 package za.co.wethinkcode.ServerTest;
 
 import org.junit.jupiter.api.Test;
+import za.co.wethinkcode.Server.Command;
 import za.co.wethinkcode.Server.World.Obstacles;
+import za.co.wethinkcode.Server.World.Position;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ObstacleTest {
@@ -45,5 +52,43 @@ public class ObstacleTest {
         Obstacles obstacle = new Obstacles(type, x, y);
 
         assertEquals(y, obstacle.getY());
+    }
+    @Test
+    void testIsPathBlockedObstacle() {
+        Command command = new Command();
+
+        // Test path is not blocked
+        Position start = new Position(0, 0);
+        Position end = new Position(10, 10);
+        assertFalse(command.IsPathBlockedObstacle(start, end));
+
+        // Test path is blocked by obstacle
+        start = new Position(2, 2);
+        end = new Position(4, 4);
+        assertFalse(command.IsPathBlockedObstacle(start, end));
+
+        // Test path is partially blocked by obstacle
+        start = new Position(0, 0);
+        end = new Position(2, 2);
+        assertFalse(command.IsPathBlockedObstacle(start, end));
+    }
+    @Test
+    void testIsRobotPathBlocked(){
+        Command command = new Command();
+
+        // Test path is not blocked by a Robot
+        Position start = new Position(0, 0);
+        Position end = new Position(10, 10);
+        assertFalse(command.IsRobotPathBlocked(start, end));
+
+        // Test path is blocked by a Robot
+        start = new Position(2, 2);
+        end = new Position(4, 4);
+        assertFalse(command.IsRobotPathBlocked(start, end));
+
+        // Test path is partially blocked by a Robot
+        start = new Position(0, 0);
+        end = new Position(2, 2);
+        assertFalse(command.IsRobotPathBlocked(start, end));
     }
 }
